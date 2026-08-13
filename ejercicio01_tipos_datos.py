ce93f8ab015ea9d32257eb65cc48af10f4bc8194
Jhostyn <jhostyngavilanez@gmail.com>
2026-08-12 22:40:33 -0500

    feat: Ejercicio de tipos de datos python

---
+++ ejercicio01_tipos_datos.py
@@ -0,0 +1,28 @@
+# Ejercicio 1: Tipos de datos en Python
+"""
+Ejercicio tipos de datos
+
+Objetivo
+
+Crear variables con diferentes tipos de datos en Python
+
+Instrucciones
+
+Crea cuatro variables en Python, cada una con un tipo de dato diferente:
+
+Una variable llamada age que contenga tu edad como número entero.
+Una variable llamada height que contenga tu altura en metros como número flotante (por ejemplo 1.75).
+Una variable llamada is_student que contenga un valor booleano (True o False) indicando si eres estudiante o no.
+Una variable llamada name que contenga tu nombre como cadena de texto.
+
+Finalmente, imprime cada variable en una línea separada usando la función print().
+"""
+age = 25
+height = 1.75
+is_student = True
+name = "Jhostyn"
+
+print("Edad:", age)
+print("Altura:", height)
+print("¿Es estudiante?:", is_student)
+print("Nombre:", name)
